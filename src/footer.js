e75dac94233826e3bbd70d0928ecad94adadbb24
unknown <bhatia2003varun@gmail.com>
2023-11-09 11:46:00 +0530

    updated

---
+++ src/footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Footer from './Footer';
+
+function App() {
+  return (
+    <div>
+      {/* Your main content */}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;

4347e7921759e472524d488348eb4fedc1f6941e
unknown <bhatia2003varun@gmail.com>
2023-12-03 13:50:38 +0530

    updated

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,23 +3,26 @@ import React from 'react'
 export default function footer() {
     return (
         <div>
-            
 
-                <div className='flex flex-row bg-blue-600 h-50 text-white py-4 bottom-0 w-full '>
-                    <div>
+
+            <div className='flex flex-row bg-blue-600 h-50 text-white py-4 bottom-0 w-full '>
+                <div>
                     <span className='underline font-bold text-2xl text-left'> PRODUCT CATEGORIES </span>
-                  <div className='text-2xl hover:text-red-400'> Smartphones  </div>
-                  <div className='text-2xl hover:text-red-400'> Televisions </div>
-                  <div className='text-2xl hover:text-red-400'> AirConditioners </div>
-                  <div className='text-2xl hover:text-red-400'> Refregirators </div>
-                  <div className='text-2xl hover:text-red-400'> Laptops </div>
-                    </div>
-
-                    <div>
-                    <span className='underline font-bold text-2xl text-white text-left pl-40'> CONNECT WITH US </span>
-                    </div>
+                    <div className='text-2xl hover:text-red-400'> Smartphones  </div>
+                    <div className='text-2xl hover:text-red-400'> Televisions </div>
+                    <div className='text-2xl hover:text-red-400'> AirConditioners </div>
+                    <div className='text-2xl hover:text-red-400'> Refregirators </div>
+                    <div className='text-2xl hover:text-red-400'> Laptops </div>
                 </div>
-            
+
+                <div>
+                    <span className='underline font-bold text-2xl text-white text-left pl-40'> CONNECT WITH US </span>
+                    <div className='text-2xl hover:text-red-400'> Facebook</div>
+                    <div className='text-2xl hover:text-red-400'> Twitter</div>
+                    <div className='text-2xl hover:text-red-400'> Instagram</div>
+                </div>
+            </div>
+
             <footer className='bg-red-900 h-50 text-white py-2 bottom-0 w-full'>
                 <div className="container mx-auto text-center">
                     <div className='underline'> Disclaimer </div>
@@ -30,6 +33,6 @@ export default function footer() {
                 </div>
             </footer>
         </div>
-        
+
     )
 }
